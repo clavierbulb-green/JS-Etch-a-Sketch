e1f912307f9326fbbeceb6be350f541dabb08e08
clavierbulb-green <clavierbulb-green@protonmail.com>
2019-12-04 14:30:17 -0800

    Refactor code + responsive UI w/ grid-size slider

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
-A browser console-based implementation of an Etch-a-Sketch.
+# Etch-A-Sketch
+---
+A browser implementation of an Etch-a-Sketch.
 
-To draw on the grid, click and hold the mouse while moving the cursor over the
-squares to color in. Clicking on a single square will also color it. To clear 
-the grid, press the CLEAR GRID button. Pressing this button will also 
-open a prompt allowing a grid with a new number of squares per side.
+Inspired by and written as a solution to the practice project
+[Etch-a-Sketch](https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project) by the Odin Project
 
-The program starts in BASIC drawing mode, where dragging the mouse changes the
-color of the squares changes directly from black to white. By pressing the
-TOGGLE MODE button, one can change between BASIC mode and FINE mode. In FINE
-mode, the color of a square changes to a lighter shade of grey each time it is
-drawn over, allowing for finer shading and expression.
+![Screenshot of Etch-A-Sketch App](assets/images/preview/screenshot.png)
 
-Inspired by and written as a solution to the practice project [Etch-a-Sketch](
-https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-
-sketch-project) by the Odin Project
+---
+## How-to-Use
+
++ Press the `DRAW` button to enter drawing-mode, and hover over the grid to mark the squares. Press the `DRAW` button again to stop drawing.
+
++ Press the `CLEAR` button to clear the grid. 
+
++ Move the `GRID-SIZE` slider to change the number of squares on the grid
+*(This will also clear any squares that have been marked).*
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,136 +1,54 @@
-//Create a 16x16 grid of square divs 
+const grid = document.getElementById("grid");
+const defaultSideLength = 24;
 
-//variables
-let numRows= 16;
-let numColumns = 16;
-let drawing = false; 
-let fineDrawing = false;
+const clearButton = document.getElementById("clear_button");
+clearButton.addEventListener("click", () => clearGrid());
 
-let grid__container = document.querySelector(".grid__container");
-
-//add functionality to buttons
-let clear__button = document.querySelector("#clear__button");
-clear__button.addEventListener("click", e => {
-    clearGrid();
-    if (prompt("Make a new grid?" + 
-    " (enter 'yes' for a new grid)").toLowerCase() === "yes") {
-        makeNewGrid(); 
-    }
+const drawButton = document.getElementById("draw_button");
+drawButton.addEventListener("click", () => {
+    drawButton.classList.toggle("active");
 });
 
-let toggle__gradient__button =
-document.querySelector("#toggle__gradient__button");
-toggle__gradient__button.addEventListener("click", e => {
-    fineDrawing = !fineDrawing;
-    if (fineDrawing) {
-        toggle__gradient__button.textContent = "TOGGLE MODE [FINE]";
-    }
-    else {
-        toggle__gradient__button.textContent = "TOGGLE MODE [BASIC]";
-    }
+const sizeSlider = document.getElementById("grid_size");
+sizeSlider.addEventListener("mouseup", () => {
+    removeSquares();
+    populateGrid(sizeSlider.value);
 });
 
-/*
-let invert__color__button = 
-document.querySelector("#invert__color__button");
-invert__color__button.addEventListener("click", e => {
-    //TODO support inverting shaded squares
-    let squares = document.querySelectorAll('.grid__container div');
-    squares.forEach(listObj => {
-        //TODO
-    });
+window.addEventListener("load", () => {
+    populateGrid(defaultSideLength);
 });
-*/
 
-/*==========FUNCTIONS==========*/
 
-function addSquares() {
-    for (let row = 1; row <= numRows; row++) {
-        for (let col = 1; col <= numColumns; col++) {
-            let square = document.createElement('div');
-            square.style["grid-column"] = col;
-            square.style["grid-row"] = row;
+function removeSquares() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
 
-            square.setAttribute("color-key", "0");
-            square.style["background-color"] = "#000000";
-            square.style.border = "1px #AAAAAA solid";
-
-            /* A square is marked by a mouse-click on the square.
-            Squares continue to be marked as the mouse hovers over them
-            as long as the mouse is down */
-            square.addEventListener("mouseenter", e => {
-                if (drawing) {
-                    if (fineDrawing) {
-                        shade(square);
-                    }
-                    else {
-                        invertColor(square);
-                    }
-                }
-            });
-            square.addEventListener("mousedown", e => {
-                drawing = true;
-                if (fineDrawing) {
-                    shade(square);
-                }
-                else {
-                    invertColor(square);
-                }
-            });
-            square.addEventListener("mouseup", e => {
-                drawing = false;
-            });
-
-            grid__container.appendChild(square);
+function clearGrid() {
+    let squares = grid.childNodes;
+    for (let i = 0; i < squares.length; i++) {
+        let square = squares[i];
+        if (square.classList.contains("marked")) {
+            square.classList.remove("marked");
         }
     }
 }
 
 
-function invertColor(square) {
-    square.setAttribute("color-key", "255");
-    square.style["background-color"] = "rgb(255, 255, 255)";
-}
-
-
-function shade(square) {
-    let colorKey = Number(square.getAttribute("color-key"))
-    // 256/32 = 8, so 9 shades total including black and white
-    if (colorKey < 255) {
-        colorKey += 32;
-        console.log(colorKey);
-        square.setAttribute("color-key", colorKey);
-
-        let colorString = 
-            "rgb(" + colorKey + ", " + colorKey + ", " + colorKey + ")";
-        square.style["background-color"] = colorString;
+function populateGrid(sideLength) {
+    grid.style["grid-template-columns"] = `repeat(${sideLength}, 1fr)`;
+    for (let row = 0; row < sideLength; row++) {
+        for (let col = 0; col < sideLength; col++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("mouseenter", () => {
+                if (drawButton.classList.contains("active")) {
+                    square.classList.add("marked");
+                }
+            });
+            grid.appendChild(square);
+        }
     }
 }
-
-
-function clearGrid() {
-    let squares = document.querySelectorAll('.grid__container div');
-    squares.forEach(listObj => {
-        listObj.setAttribute("color-key", "0");
-        listObj.style["background-color"] = "#000000";
-    });
-}
-
-
-function makeNewGrid() {
-    let newLength = 0;
-    while (newLength < 2 || newLength >= 100) {
-        newLength = prompt("How many squares per side to make the new grid?" +
-        "(Enter a length between 2 and 100)");
-    }
-    numRows = newLength;
-    numColumns = newLength;
-
-    let squares = document.querySelectorAll('.grid__container div');
-    squares.forEach(listObj => {
-        listObj.remove();
-    });
-    addSquares();
-}
-
-addSquares();
